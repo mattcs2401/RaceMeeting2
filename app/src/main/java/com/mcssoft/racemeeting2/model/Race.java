2616c4eae8dc7d057ae2799d88dd79addd3bb5a9
mattcs2401 <mattcs2401@gmail.com>
2017-03-27 09:18:17 +1000

    Start of RaceMeeting2.

---
+++ app/src/main/java/com/mcssoft/racemeeting2/model/Race.java
@@ -0,0 +1,48 @@
+package com.mcssoft.racemeeting2.model;
+
+/**
+ * Class to model a race.
+ */
+public class Race {
+
+}
+/*
+ Example
+ -------
+
+ Using: https://tatts.com/pagedata/racing/2017/3/27/RaceDay.xml
+
+ <Race RaceDisplayStatus="SELLING"
+       RaceNo="1"
+       RaceTime="2017-03-27T12:55:00"
+       RaceName="BM 60 HANDICAP"
+       Distance="1905"
+       SubFav="0"
+       WeatherChanged="N"
+       WeatherCond="2"
+       WeatherDesc="Overcast"
+       TrackChanged="N"
+       TrackCond="5"
+       TrackDesc="Heavy"
+       TrackRating="9"
+       TrackRatingChanged="Y">
+*/
+
+/*
+ Example
+ -------
+
+ Using: Using: https://tatts.com/pagedata/racing/2017/3/27/NR.xml
+
+ <Race RaceNo="1"
+       RaceTime="2017-03-27T12:55:00"
+       RaceName="BM 60 HANDICAP"
+       Distance="1905"
+       RaceDisplayStatus="SELLING"
+       WeatherChanged="N"
+       WeatherCond="2"
+       WeatherDesc="Overcast"
+       TrackChanged="N"
+       TrackCond="5"
+       TrackDesc="Heavy">
+ */
