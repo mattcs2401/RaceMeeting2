c5ab40ad377a5e9db985a751a0de7b8436428e49
mattcs2401 <mattcs2401@gmail.com>
2017-03-27 12:03:42 +1000

    Start of RaceMeetings2.

---
+++ app/src/main/java/com/mcssoft/racemeetings2/model/Runner.java
@@ -0,0 +1,45 @@
+package com.mcssoft.racemeetings2.model;
+
+/**
+ * Class to model a race runner.
+ */
+public class Runner {
+}
+
+/*
+ Example
+ -------
+
+ Using: https://tatts.com/pagedata/racing/2017/3/27/NR.xml
+
+ <meeting ...
+   <race ...
+     <Runner RunnerNo="1" RunnerName="MY COUSIN JACKIE" Barrier="3" JockeySilk="57804"/>
+     <Runner RunnerNo="2" RunnerName="FRENCH SONG" Barrier="2" JockeySilk="52395"/>
+     <Runner RunnerNo="3" RunnerName="CANTILLATE" Barrier="1" JockeySilk="6809"/>
+     <Runner RunnerNo="4" RunnerName="FAVULOUS FLOYD" Barrier="5" JockeySilk="9043"/>
+     <Runner RunnerNo="5" RunnerName="SOUND WORLD" Barrier="4" JockeySilk="56405"/>
+     <Runner RunnerNo="7" RunnerName="PREMIERE PLACE" Barrier="6" JockeySilk="9638"/>
+     <Runner RunnerNo="6" RunnerName="GRAND DAZZLER" Barrier="7" Scratched="Y" ScratchStatus="3" JockeySilk="65535"/>
+*/
+/*
+ Example:
+ --------
+
+ Using: https://tatts.com/pagedata/racing/2017/3/27/NR1.xml
+
+ <meeting ...
+   <race ...
+     <Runner RunnerNo="1"
+             RunnerName="MY COUSIN JACKIE"
+             Scratched="N"
+             Rider="J MURPHY(A)"
+             RiderChanged="N"
+             Barrier="3"
+             Handicap="0"
+             Weight="62.0"
+             Form="WC"
+             LastResult="801"
+             Rtng="95"
+             JockeySilk="57804">
+*/
