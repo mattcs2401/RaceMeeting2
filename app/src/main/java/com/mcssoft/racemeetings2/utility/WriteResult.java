906f154f2bf80a8c44ed5308d600b36adc59f127
mattcs2401 <mattcs2401@gmail.com>
2017-04-12 13:50:34 +1000

    Minor code changes to WriteResult.

--- app/src/main/java/com/mcssoft/racemeetings2/utility/WriteResult.java
+++ app/src/main/java/com/mcssoft/racemeetings2/utility/WriteResult.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import com.mcssoft.racemeetings2.database.DatabaseOperations;
 import com.mcssoft.racemeetings2.database.SchemaConstants;
 import com.mcssoft.racemeetings2.interfaces.IWriteResult;
+import com.mcssoft.racemeetings2.model.Meeting;
 
 import java.util.List;
 
@@ -14,18 +16,18 @@ import java.util.List;
  * Utility class to write a list of one or more objects into the respective table.
  */
 public class WriteResult extends AsyncTask<List,Void,String> {
-
     /**
      * Constructor.
-     * @param context The app cpntext.
-     * @param message A message for the progress dialog.
-     * @param input   The data to process.
-     * @param inputType An indicator of what the input is associated with.
+     *
+     * @param context   The app cpntext.
+     * @param message   A message for the progress dialog.
+     * @param input     The data to process.
+     * @param inputType An indicator of what the inputList is associated with.
      */
     public WriteResult(Context context, String message, List input, String inputType) {
         this.context = context;
         this.message = message;
-        this.input = input;
+        this.inputList = input;
         this.inputType = inputType;
     }
 
@@ -42,13 +44,15 @@ public class WriteResult extends AsyncTask<List,Void,String> {
     protected String doInBackground(List... params) {
         String theResult = null;
         try {
-            switch(inputType) {
+            switch (inputType) {
                 case SchemaConstants.MEETINGS_TABLE:
+                    checkOrUpdateMeetings();
                     break;
                 case SchemaConstants.RACES_TABLE:
+                    checkOrUpdateRaces();
                     break;
             }
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
         }
         return theResult;
@@ -63,30 +67,28 @@ public class WriteResult extends AsyncTask<List,Void,String> {
         writeResult.writeResult(inputType, theResult);
     }
 
+    private void checkOrUpdateMeetings() {
+        DatabaseOperations dbOper = new DatabaseOperations(context);
+        for(Object object : inputList) {
+            Meeting meeting = ((Meeting) object);
+            String meetingId = meeting.getMeetingId();
+            if(!dbOper.checkRecordExists(SchemaConstants.MEETINGS_TABLE, SchemaConstants.MEETING_ID,
+                    meetingId)) {
+                dbOper.insertMeetingRecord(meeting);
+            }
+        }
+    }
+
+    private void checkOrUpdateRaces() {
+        // TBA
+    }
+
+
     public IWriteResult writeResult = null;
 
-    private List input;
+    private List inputList;
     private String inputType;
     private String message;
     private Context context;
     private ProgressDialog progressDialog;
 }
-/*
-    private void checkOrUpdateMeetings(List resultList) {
-        DatabaseOperations dbOper = new DatabaseOperations(this);
-        for(Object meeting : resultList) {
-            //meetingIdList.add(((Meeting) meeting).getMeetingId());
-            String meetingId = ((Meeting) meeting).getMeetingId();
-            if(!dbOper.checkRecordExists(SchemaConstants.MEETINGS_TABLE, SchemaConstants.MEETING_ID,
-                    meetingId)) {
-                dbOper.insertMeetingRecord((Meeting) meeting);
-                // TBA - insert meeting record.
-                String bp = "";
-
-            }
-        }
-
-
-        String bp = "";
-    }
-*/
