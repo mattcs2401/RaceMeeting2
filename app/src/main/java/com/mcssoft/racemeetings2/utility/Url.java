f432f767e47fdae69a723f1a30967e87202636fc
mattcs2401 <mattcs2401@gmail.com>
2017-05-05 13:23:02 +1000

    Updates to create Race number Url.

--- app/src/main/java/com/mcssoft/racemeetings2/utility/Url.java
+++ app/src/main/java/com/mcssoft/racemeetings2/utility/Url.java
@@ -16,31 +16,68 @@ public class Url {
 
     public void Url() { }
 
-    public String createRaceDayUrl(@Nullable String[] date) {
-        if(date == null) {
-            date = getDateComponents(null);
+    /**
+     * Create the RaceDay Url
+     * https://tatts.com/pagedata/racing/YYYY/M(M)/D(D)/RaceDay.xml
+     * @param raceDate Optional date value. If not set then current date used.
+     * @return The Url.
+     */
+    public String createRaceDayUrl(@Nullable String[] raceDate) {
+        if(raceDate == null) {
+            raceDate = getDateComponents(null);
         }
         Uri.Builder builder = new Uri.Builder();
         builder.encodedPath(Resources.getInstance().getString(R.string.base_path))
-                .appendPath(date[0])
-                .appendPath(date[1])
-                .appendPath(date[2])
+                .appendPath(raceDate[0])
+                .appendPath(raceDate[1])
+                .appendPath(raceDate[2])
                 .appendPath(Resources.getInstance().getString(R.string.race_day_listing));
         builder.build();
         return builder.toString();
     }
 
-    public String createMeetingUrl(String[] date, String code) {
+    /**
+     * Create the Meeting Url
+     * https://tatts.com/pagedata/racing/YYYY/M(M)/D(D)/code.xml
+     * @param meetingDate The meeting date value.
+     * @param meetingCode The meeting code value.
+     * @return The Url.
+     */
+    public String createMeetingUrl(String[] meetingDate, String meetingCode) {
         Uri.Builder builder = new Uri.Builder();
         builder.encodedPath(Resources.getInstance().getString(R.string.base_path))
-                .appendPath(date[0])
-                .appendPath(date[1])
-                .appendPath(date[2])
-                .appendPath(code + ".xml");
+                .appendPath(meetingDate[0])
+                .appendPath(meetingDate[1])
+                .appendPath(meetingDate[2])
+                .appendPath(meetingCode + ".xml");
         builder.build();
         return builder.toString();
     }
 
+    /**
+     * Create the Race Url.
+     * https://tatts.com/pagedata/racing/YYYY/M(M)/D(D)/code+number.xml
+     * @param meetingDate The meeting date value.
+     * @param meetingCode The meeting code value.
+     * @param raceNo The race number.
+     * @return The Url
+     */
+    public String createRaceUrl(String[] meetingDate, String meetingCode, String raceNo) {
+        Uri.Builder builder = new Uri.Builder();
+        builder.encodedPath(Resources.getInstance().getString(R.string.base_path))
+                .appendPath(meetingDate[0])
+                .appendPath(meetingDate[1])
+                .appendPath(meetingDate[2])
+                .appendPath(meetingCode + raceNo + ".xml");
+        builder.build();
+        return builder.toString();
+    }
+
+    /**
+     * generate the YYYY, MM, DD elements of a meeting date.
+     * @param raceDate Optional date value. If not set then current date used.
+     * @return The date as: [0]-YYYY, [1]-M(M), [2]-D(D)
+     */
     public String[] getDateComponents(@Nullable String raceDate) {
         String[] date = new String[3];
         if(raceDate == null) {
