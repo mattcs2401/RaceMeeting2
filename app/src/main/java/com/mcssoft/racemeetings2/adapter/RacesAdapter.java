deff802268ea75ca0cf1bf0a1c2cb05277f19d4d
mattcs2401 <mattcs2401@gmail.com>
2017-05-08 12:51:05 +1000

    Updates to finish Race adapter and viewholder.

--- app/src/main/java/com/mcssoft/racemeetings2/adapter/RacesAdapter.java
+++ app/src/main/java/com/mcssoft/racemeetings2/adapter/RacesAdapter.java
@@ -2,16 +2,26 @@ package com.mcssoft.racemeetings2.adapter;
 
 import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
-import com.mcssoft.racemeetings2.interfaces.IMeetingItemClickListener;
+import com.mcssoft.racemeetings2.R;
+import com.mcssoft.racemeetings2.database.SchemaConstants;
 import com.mcssoft.racemeetings2.interfaces.IRaceItemClickListener;
 
 public class RacesAdapter extends RecyclerView.Adapter<RacesViewHolder> {
 
     @Override
     public RacesViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        if ( parent instanceof RecyclerView ) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.race_row, parent, false);
+            view.setFocusable(true);
+            // don't need to keep a local copy, framework now supplies.
+            return new RacesViewHolder(view, itemClickListener); //, itemLongClickListener);
+        } else {
+            throw new RuntimeException("Not bound to RecyclerView");
+        }
     }
 
     @Override
@@ -21,13 +31,15 @@ public class RacesAdapter extends RecyclerView.Adapter<RacesViewHolder> {
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
-
-    private void adapaterOnBindViewHolder(RacesViewHolder holder, int position) {
-//        cursor.moveToPosition(position);
-//        holder.getTvMeetingCode().setText(cursor.getString(meetingCodeNdx));
-//        holder.getTvVenueName().setText(cursor.getString(meetingVenueNdx));
+        if(isEmptyView) {
+            return  1; // need to do this so the onCreateViewHolder fires.
+        } else {
+            if(cursor != null) {
+                return cursor.getCount();
+            } else {
+                return 0;
+            }
+        }
     }
 
     @Override
@@ -40,11 +52,11 @@ public class RacesAdapter extends RecyclerView.Adapter<RacesViewHolder> {
         cursor = newCursor;
         if(cursor != null) {
             cursor.moveToFirst();
-
-//        idColNdx = cursor.getColumnIndex(SchemaConstants.MEETING_ROWID);
-//        meetingCodeNdx = cursor.getColumnIndex(SchemaConstants.MEETING_CODE);
-//        meetingVenueNdx = cursor.getColumnIndex(SchemaConstants.MEETING_VENUE);
-
+            idColNdx = cursor.getColumnIndex(SchemaConstants.RACE_ROWID);
+            raceNoColNdx = cursor.getColumnIndex(SchemaConstants.RACE_NO);
+            raceNameColNdx = cursor.getColumnIndex(SchemaConstants.RACE_NAME);
+            raceTimeColNdx = cursor.getColumnIndex(SchemaConstants.RACE_TIME);
+            raceDistColNdx = cursor.getColumnIndex(SchemaConstants.RACE_DIST);
             notifyDataSetChanged();
         }
     }
@@ -56,11 +68,26 @@ public class RacesAdapter extends RecyclerView.Adapter<RacesViewHolder> {
     }
 
     public void setEmptyView(boolean emptyView) {
-        this.emptyView = emptyView;
+        this.isEmptyView = emptyView;
     }
 
-    private boolean emptyView;
+    private void adapaterOnBindViewHolder(RacesViewHolder holder, int position) {
+        cursor.moveToPosition(position);
+        holder.getTvRaceNo().setText(cursor.getString(raceNoColNdx));
+        holder.getTvRaceName().setText(cursor.getString(raceNameColNdx));
+        holder.getTvRaceTime().setText(cursor.getString(raceTimeColNdx));
+        holder.getTvRaceDist().setText(cursor.getString(raceDistColNdx));
+    }
+
     private int idColNdx;
+    private int raceNoColNdx;
+    private int raceNameColNdx;
+    private int raceTimeColNdx;
+    private int raceDistColNdx;
+
+    private View view;
     private Cursor cursor;
+    private boolean isEmptyView;
+
     private IRaceItemClickListener itemClickListener;
 }
